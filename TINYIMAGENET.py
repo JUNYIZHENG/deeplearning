6de6c1f283fcfed3904ec120c84432117c73ae16
Junyi Zheng <38276769+JUNYIZHENG@users.noreply.github.com>
2019-10-12 19:02:57 -0500

    Update TINYIMAGENET.py

--- TINYIMAGENET.py
+++ TINYIMAGENET.py
@@ -111,7 +111,7 @@ transform_train = transforms.Compose([transforms.RandomCrop(32, 4), transforms.R
                                       transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 transform_val = transforms.Compose([transforms.ToTensor()])
 
-train_dir = '/u/training/tra216/scratch/tiny-imagenet-200/train'
+train_dir = '/u/training/tra216/scratch/tiny-imagenet-200/train/'
 train_dataset = datasets.ImageFolder(train_dir,
          transform=transform_train)
 #print(train_dataset.class_to_idx)
