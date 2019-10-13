17878b56718a47625786180335a90bc40c53efbb
Junyi Zheng <38276769+JUNYIZHENG@users.noreply.github.com>
2019-10-13 17:27:22 -0500

    Update sync.py

--- sync.py
+++ sync.py
@@ -127,12 +127,12 @@ transform_test = transforms.Compose([transforms.ToTensor(),
                                      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
 # For trainning data
-trainset = torchvision.datasets.CIFAR100(root=’~/scratch/’,
+trainset = torchvision.datasets.CIFAR100(root=’/u/training/tra216/scratch/’,
         train=True,download=False, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset,
         batch_size=100, shuffle=True, num_workers=8)
 # For testing data
-testset = torchvision.datasets.CIFAR100(root=’~/scratch/’,
+testset = torchvision.datasets.CIFAR100(root=’/u/training/tra216/scratch/’,
         train=False,download=False, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset,
         batch_size=100, shuffle=False, num_workers=8)
