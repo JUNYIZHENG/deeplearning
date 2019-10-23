4b8f83c40d4de3482d5530550214f04e9ddc0149
Junyi Zheng <38276769+JUNYIZHENG@users.noreply.github.com>
2019-10-23 03:39:57 -0500

    Update RNN_sentiment_analysis.py

--- NLP-Movie Sentiment Analysis/2a/RNN_sentiment_analysis.py
+++ NLP-Movie Sentiment Analysis/2a/RNN_sentiment_analysis.py
@@ -68,7 +68,7 @@ L_Y_test = len(y_test)
 
 train_loss = []
 train_accu = []
-test_accu = []
+# test_accu = []
 
 
 # training
@@ -124,63 +124,63 @@ for epoch in range(no_of_epochs):
     print(epoch, "%.2f" % (epoch_acc * 100.0), "%.4f" %
           epoch_loss, "%.4f" % float(time.time() - time1))
 
-    if ((epoch + 1) % 3) == 0:
-        # test
-        sequence_length = 200
-        model.eval()
+#     if ((epoch + 1) % 3) == 0:
+#         # test
+#         sequence_length = 200
+#         model.eval()
 
-        epoch_acc = 0.0
-        epoch_loss = 0.0
+#         epoch_acc = 0.0
+#         epoch_loss = 0.0
 
-        epoch_counter = 0
+#         epoch_counter = 0
 
-        time1 = time.time()
+#         time1 = time.time()
 
-        I_permutation = np.random.permutation(L_Y_test)
+#         I_permutation = np.random.permutation(L_Y_test)
 
 
-        for i in range(0, L_Y_test, batch_size):
+#         for i in range(0, L_Y_test, batch_size):
 
-            x_input2 = [x_test[j] for j in I_permutation[i:i + batch_size]]
-            x_input = np.zeros((batch_size, sequence_length), dtype=np.int)
-            for j in range(batch_size):
-                x = np.asarray(x_input2[j])
-                sl = x.shape[0]
-                if (sl < sequence_length):
-                    x_input[j, 0:sl] = x
-                else:
-                    start_index = np.random.randint(sl - sequence_length + 1)
-                    x_input[j, :] = x[start_index:(start_index + sequence_length)]
-            y_input = y_test[I_permutation[i:i + batch_size]]
-            data = Variable(torch.LongTensor(x_input)).cuda()
-            target = Variable(torch.FloatTensor(y_input)).cuda()
+#             x_input2 = [x_test[j] for j in I_permutation[i:i + batch_size]]
+#             x_input = np.zeros((batch_size, sequence_length), dtype=np.int)
+#             for j in range(batch_size):
+#                 x = np.asarray(x_input2[j])
+#                 sl = x.shape[0]
+#                 if (sl < sequence_length):
+#                     x_input[j, 0:sl] = x
+#                 else:
+#                     start_index = np.random.randint(sl - sequence_length + 1)
+#                     x_input[j, :] = x[start_index:(start_index + sequence_length)]
+#             y_input = y_test[I_permutation[i:i + batch_size]]
+#             data = Variable(torch.LongTensor(x_input)).cuda()
+#             target = Variable(torch.FloatTensor(y_input)).cuda()
 
-            # x_input = [x_test[j] for j in I_permutation[i:i + batch_size]]
-            # y_input = np.asarray([y_test[j] for j in I_permutation[i:i + batch_size]], dtype=np.int)
-            # target = Variable(torch.FloatTensor(y_input)).cuda()
+#             # x_input = [x_test[j] for j in I_permutation[i:i + batch_size]]
+#             # y_input = np.asarray([y_test[j] for j in I_permutation[i:i + batch_size]], dtype=np.int)
+#             # target = Variable(torch.FloatTensor(y_input)).cuda()
 
-            with torch.no_grad():
-                loss, pred = model(x_input, target)
+#             with torch.no_grad():
+#                 loss, pred = model(x_input, target)
 
-            prediction = pred >= 0.0
-            truth = target >= 0.5
-            acc = prediction.eq(truth).sum().cpu().data.numpy()
-            epoch_acc += acc
-            epoch_loss += loss.data.item()
-            epoch_counter += batch_size
+#             prediction = pred >= 0.0
+#             truth = target >= 0.5
+#             acc = prediction.eq(truth).sum().cpu().data.numpy()
+#             epoch_acc += acc
+#             epoch_loss += loss.data.item()
+#             epoch_counter += batch_size
 
-        epoch_acc /= epoch_counter
-        epoch_loss /= (epoch_counter / batch_size)
+#         epoch_acc /= epoch_counter
+#         epoch_loss /= (epoch_counter / batch_size)
 
-        test_accu.append(epoch_acc)
+#         test_accu.append(epoch_acc)
 
-        time2 = time.time()
-        time_elapsed = time2 - time1
+#         time2 = time.time()
+#         time_elapsed = time2 - time1
 
-        print("  ", "%.2f" % (epoch_acc * 100.0), "%.4f" % epoch_loss)
+#         print("  ", "%.2f" % (epoch_acc * 100.0), "%.4f" % epoch_loss)
 
 
 torch.save(model, 'rnn.model')
-data = [train_loss, train_accu, test_accu]
+data = [train_loss, train_accu]
 data = np.asarray(data)
 np.save('data.npy', data)
