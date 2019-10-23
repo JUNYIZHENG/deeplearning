b1da198ad6e9f3a623368e43588788d0bdd5ab54
Junyi Zheng <38276769+JUNYIZHENG@users.noreply.github.com>
2019-10-23 03:28:34 -0500

    Update RNN_sentiment_analysis.py

--- NLP-Movie Sentiment Analysis/2a/RNN_sentiment_analysis.py
+++ NLP-Movie Sentiment Analysis/2a/RNN_sentiment_analysis.py
@@ -141,7 +141,7 @@ for epoch in range(no_of_epochs):
 
         for i in range(0, L_Y_test, batch_size):
 
-            x_input2 = [x_train[j] for j in I_permutation[i:i + batch_size]]
+            x_input2 = [x_test[j] for j in I_permutation[i:i + batch_size]]
             x_input = np.zeros((batch_size, sequence_length), dtype=np.int)
             for j in range(batch_size):
                 x = np.asarray(x_input2[j])
@@ -151,7 +151,7 @@ for epoch in range(no_of_epochs):
                 else:
                     start_index = np.random.randint(sl - sequence_length + 1)
                     x_input[j, :] = x[start_index:(start_index + sequence_length)]
-            y_input = y_train[I_permutation[i:i + batch_size]]
+            y_input = y_test[I_permutation[i:i + batch_size]]
             data = Variable(torch.LongTensor(x_input)).cuda()
             target = Variable(torch.FloatTensor(y_input)).cuda()
 
