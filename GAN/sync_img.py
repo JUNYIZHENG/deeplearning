c19202a06ee51d2beaa0475e3204ce01b5e5390d
Junyi Zheng <38276769+JUNYIZHENG@users.noreply.github.com>
2019-11-10 22:44:58 -0600

    Update sync_img.py

--- GAN/sync_img.py
+++ GAN/sync_img.py
@@ -9,9 +9,65 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
-from gan import *
 from torch import autograd
 from torch.autograd import Variable
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=196, kernel_size=3, stride=1, padding=1),
+            nn.LayerNorm(normalized_shape=[196, 32, 32]),
+            nn.LeakyReLU(0.02, inplace=True)
+            )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(196, 196, 3, 2, 1),
+            nn.LayerNorm(normalized_shape=[196, 16, 16]),
+            nn.LeakyReLU(0.02, inplace=True))
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(196, 196, 3, 1, 1),
+            nn.LayerNorm(normalized_shape=[196, 16, 16]),
+            nn.LeakyReLU(0.02, inplace=True))
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(196, 196, 3, 2, 1),
+            nn.LayerNorm(normalized_shape=[196, 8, 8]),
+            nn.LeakyReLU(0.02, inplace=True))
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(196, 196, 3, 1, 1),
+            nn.LayerNorm(normalized_shape=[196, 8, 8]),
+            nn.LeakyReLU(0.02, inplace=True))
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(196, 196, 3, 1, 1),
+            nn.LayerNorm(normalized_shape=[196, 8, 8]),
+            nn.LeakyReLU(0.02, inplace=True))
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(196, 196, 3, 1, 1),
+            nn.LayerNorm(normalized_shape=[196, 8, 8]),
+            nn.LeakyReLU(0.02, inplace=True))
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(196, 196, 3, 2, 1),
+            nn.LayerNorm(normalized_shape=[196, 4, 4]),
+            nn.LeakyReLU(0.02, inplace=True))
+        self.pool = nn.MaxPool2d(kernel_size=4)
+        self.fc1 = nn.Sequential(
+            nn.Linear(196, 1))
+        self.fc10 = nn.Sequential(
+            nn.Linear(196, 10)
+            )
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
+        x = self.pool(x)
+        x = x.view(-1, x.size()[1]*x.size()[2]*x.size()[3])
+        fc1_out = self.fc1(x)
+        fc10_out = self.fc10(x)
+        return fc1_out, fc10_out
 
 def plot(samples):
     fig = plt.figure(figsize=(10, 10))
