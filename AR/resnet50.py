df1d6c70962814b8936dc2ca88d072185e4f4876
Junyi Zheng <38276769+JUNYIZHENG@users.noreply.github.com>
2019-12-11 20:40:29 -0600

    Update resnet50.py

--- AR/resnet50.py
+++ AR/resnet50.py
@@ -1,5 +1,5 @@
-import wget
- 
+import urllib 
+import urllib2 
+import requests   
 url = "https://drive.google.com/open?id=18KktApSEhzTeaLfitWRyAaJkxGstrrp2"
- 
-wget.download(url, '/u/training/tra216/scratch/hw9')
+urllib.urlretrieve(url, "/u/training/tra216/scratch/hw9")   
