8524080e62226f275ae1e3fb1e67d498ac0b048e
Junyi Zheng <38276769+JUNYIZHENG@users.noreply.github.com>
2019-12-13 17:06:09 -0600

    Delete resnet50.py

--- AR/resnet50.py
+++
@@ -1,5 +0,0 @@
-import urllib 
-import urllib2 
-import requests   
-url = "https://drive.google.com/open?id=18KktApSEhzTeaLfitWRyAaJkxGstrrp2"
-urllib.urlretrieve(url, "/u/training/tra216/scratch/hw9")   
